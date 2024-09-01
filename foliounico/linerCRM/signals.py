1a4f10832d34a8e18dcede4b698544ca8b118d78
Arturo <arturo@astrasi.com>
2024-09-01 12:47:21 -0600

    Adding delete project group association

--- foliounico/linerCRM/signals.py
+++ foliounico/linerCRM/signals.py
@@ -22,29 +22,30 @@ def generar_recibo_pdf(sender, instance, **kwargs):
         p = canvas.Canvas(buffer, pagesize=(ancho_recibo, largo_recibo))
 
         # Agregar la cabecera "JAMATIC"
-        p.setFont("Helvetica-Bold", 14)
-        p.drawCentredString(ancho_recibo / 2, largo_recibo - 20 * mm, "JAMATIC")
+        p.setFont("Helvetica-Bold", 12)
+        p.drawCentredString(ancho_recibo / 2, largo_recibo - 10 * mm, "Recibo pago")
+        p.drawCentredString(ancho_recibo / 2, largo_recibo - 20 * mm, "Servicio de Internet")
 
         # Dibujar una línea debajo de la cabecera
         p.setLineWidth(0.5)
         p.setStrokeColor(colors.black)
-        p.line(10 * mm, largo_recibo - 22 * mm, ancho_recibo - 10 * mm, largo_recibo - 22 * mm)
+        p.line(10 * mm, largo_recibo - 32 * mm, ancho_recibo - 10 * mm, largo_recibo - 32 * mm)
 
         # Configurar el estilo para el resto del texto
         p.setFont("Helvetica", 10)
 
         # Añadir el contenido del recibo
-        p.drawString(10 * mm, largo_recibo - 30 * mm, f"Recibo: {instance.recibo}")
-        p.drawString(10 * mm, largo_recibo - 40 * mm, f"Cliente: {instance.cliente.nombre_completo}")
-        p.drawString(10 * mm, largo_recibo - 50 * mm, f"Fecha: {instance.fecha_pago.strftime('%d/%m/%Y')}")
-        p.drawString(10 * mm, largo_recibo - 60 * mm, f"Monto: ${instance.monto:.2f}")
+        p.drawString(10 * mm, largo_recibo - 40 * mm, f"Recibo: {instance.recibo}")
+        p.drawString(10 * mm, largo_recibo - 50 * mm, f"Cliente: {instance.cliente.nombre_completo}")
+        p.drawString(10 * mm, largo_recibo - 60 * mm, f"Fecha: {instance.fecha_pago.strftime('%d/%m/%Y')}")
+        p.drawString(10 * mm, largo_recibo - 70 * mm, f"Monto: ${instance.monto:.2f}")
 
         # Dibujar una línea divisoria
-        p.line(10 * mm, largo_recibo - 65 * mm, ancho_recibo - 10 * mm, largo_recibo - 65 * mm)
+        p.line(10 * mm, largo_recibo - 80 * mm, ancho_recibo - 10 * mm, largo_recibo - 80 * mm)
 
         # Añadir más detalles del recibo si es necesario
-        p.drawString(10 * mm, largo_recibo - 75 * mm, "Gracias por su pago.")
-        p.drawString(10 * mm, largo_recibo - 85 * mm, "¡Visítenos nuevamente!")
+        p.drawString(10 * mm, largo_recibo - 90 * mm, "Gracias por su pago.")
+        p.drawString(10 * mm, largo_recibo - 100 * mm, "¡Visítenos nuevamente!")
 
         # Finalizar el PDF
         p.showPage()
